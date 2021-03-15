eefc087469c8935d1114e4e8138cf08c2c7940e2
brunogleite <brunoleite6o@gmail.com>
2021-03-15 13:43:20 +0000

    adding audio tags, styling class playing and adding some lines of function

--- app.js
+++ app.js
@@ -1,7 +1,11 @@
 window.addEventListener("keydown", function(e){
     const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`);
+    const key = document.querySelector(`.key[data-key="${e.keyCode}"]`);
     console.log(audio);
 
    if(!audio) return;
-   audio.play()
+   audio.currentTime = 0;
+   audio.play();
+   
+   key.classList.add("playing")
 })
