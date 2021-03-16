0baeea4c7468d3a44273cb0f5d88f6a59d0a8ade
brunogleite <brunoleite6o@gmail.com>
2021-03-16 08:53:34 +0000

    js completed

--- app.js
+++ app.js
@@ -7,5 +7,13 @@ window.addEventListener("keydown", function(e){
    audio.currentTime = 0;
    audio.play();
    
-   key.classList.add("playing")
-})
+   key.classList.add("playing");
+
+    function removeTransition(e){
+        if(e.propertyName !== "transform") return; // skip it if its not a transform 
+        this.classList.remove("playing")
+    }
+
+   const keys = document.querySelectorAll(".key");
+   keys.forEach(key => key.addEventListener('transitionend', removeTransition));
+});
