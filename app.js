176be7f1502944ccb5d3cb818384eda889cb46cc
brunogleite <brunoleite6o@gmail.com>
2021-03-16 09:32:26 +0000

    adding comments and resolving again javascript file

--- app.js
+++ app.js
@@ -1,19 +1,19 @@
 window.addEventListener("keydown", function(e){
     const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`);
     const key = document.querySelector(`.key[data-key="${e.keyCode}"]`);
-    console.log(audio);
-
-   if(!audio) return;
-   audio.currentTime = 0;
-   audio.play();
-   
-   key.classList.add("playing");
-
-    function removeTransition(e){
-        if(e.propertyName !== "transform") return; // skip it if its not a transform 
-        this.classList.remove("playing")
-    }
-
-   const keys = document.querySelectorAll(".key");
-   keys.forEach(key => key.addEventListener('transitionend', removeTransition));
+    
+    if(!audio) return; //stop the function running all together
+    audio.currentTime = 0; //rewind to the start 
+    key.classList.add("playing");
+    audio.play()
 });
+
+function removeTransition(e){
+    if(e.propertyName !== "transform") return; //skip it if its not a transform
+    console.log(this);
+    this.classList.remove("playing");
+}
+//getting the key button and put transition end on animation for every key
+const keys = document.querySelectorAll(".key");
+keys.forEach(key => key.addEventListener("transitionend", removeTransition));
+
